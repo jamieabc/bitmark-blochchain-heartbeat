7d7f4c22d850eb83b7b9fa6d3985cb44ad215efa
Aaron Chang <xesuschang@gmail.com>
2019-03-10 17:13:28 +0800

    add background task runner

---
+++ background-task.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"time"
+
+	"github.com/bitmark-inc/bitmark-sdk-go/account"
+)
+
+func runBackground(interval time.Duration, accounts []account.Account) {
+	go func(time.Duration) {
+		for {
+			select {
+			case <-time.After(interval):
+				createIssuanceFromRandomAccounts(accounts)
+			}
+		}
+	}(interval)
+}
