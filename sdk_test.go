1d089aa5c77aef6555bfa41283a0435b8ef3984c
Aaron Chang <xesuschang@gmail.com>
2019-04-05 15:00:50 +0800

    add sdk test

---
+++ sdk_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"math/rand"
+	"testing"
+)
+
+const (
+	maximumNameLength = 64
+)
+
+var (
+	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+)
+
+func randomString(length int) string {
+	str := make([]rune, length)
+	for i := range str {
+		str[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(str)
+}
+
+func TestTruncateLongString(t *testing.T) {
+	longStr := randomString(2 * maximumNameLength)
+	shortStr := randomString(maximumNameLength - 1)
+	fixture := []struct {
+		str      string
+		expected string
+	}{
+		{longStr, longStr[0:maximumNameLength]},
+		{shortStr, shortStr},
+	}
+
+	for i, s := range fixture {
+		actual := truncateLongString(s.str)
+		if actual != s.expected {
+			t.Errorf("%d testerror truncated string, expected %s but get %s",
+				i,
+				s.expected,
+				actual,
+			)
+		}
+	}
+}
